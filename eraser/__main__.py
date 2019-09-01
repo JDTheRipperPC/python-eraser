e6e56e3592520894bed9d9fa47eb72bce6a97c81
Jose David JD The Ripper PC Pérez Cañellas <jdtheripperpc@gmail.com>
2019-09-01 18:34:20 +0200

    Updated eraser cli

--- eraser/__main__.py
+++ eraser/__main__.py
@@ -2,29 +2,64 @@ import argparse
 import logging
 import logging.config
 
-# TODO: fix config file
-#logging.config.fileConfig(fname='setup.cfg', disable_existing_loggers=False)
+from eraser.eraser import Eraser
+
 LOGGER = logging.getLogger(__name__)
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Eraser target files and directories')
-
     subparsers = parser.add_subparsers(help='sub-command help')
 
+    # Group: logging
+    log_group = parser.add_argument_group('logging')
+    log_group.add_argument('--loglevel', default='error', help='Logging ERROR by default',
+                           choices=['debug', 'info', 'warning', 'error', 'critical'])
+    log_group.add_argument('--logformat', default='%(asctime)s - %(levelname)s - %(message)s')
+
+    # Group: retry
+    retry_group = parser.add_argument_group('retry')
+    retry_group.add_argument('--sleep', type=int, default=5,
+                             help='Time to sleep the thread, 5 by default')
+    retry_group.add_argument('--max-retry', type=int, default=100,
+                             help='How many times will attempt to erase the files, 100 by default')
+
+    # Subparser: targets
     parser_targets = subparsers.add_parser('targets', help='targets help')
     parser_targets.add_argument(
         'targets', metavar='targets', type=str, nargs='+',
         help='List of targets files and directories to be erased')
 
-    # TODO: Add more subparsers
+    # Subparser: from_file
+    parser_from_file = subparsers.add_parser('from_file', help='targets from file help')
+    parser_from_file.add_argument('path', nargs=1, help='Path to the input file with the targets')
+    parser_from_file.add_argument('format', choices=['plain', 'json', 'xml'],
+                                  help='File format of the input file: plain, json, xml')
 
     return parser.parse_args()
 
 
+def config_logger(args):
+    numeric_level = getattr(logging, args.loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: {}'.format(args.loglevel))
+
+    log_format = args.logformat
+    if log_format is None or len(log_format) == 0:
+        raise ValueError('Invalid log format: "{}"'.format(log_format))
+
+    logging.basicConfig(level=numeric_level, format=log_format)
+
+
 def main():
     args = parse_arguments()
+    config_logger(args)
     LOGGER.debug(args)
+
+    if args.targets:
+        eraser = Eraser()
+        eraser.targets(args.targets)
+        eraser.erase()
 
 
 if __name__ == '__main__':
--- eraser/eraser.py
+++ eraser/eraser.py
@@ -1,15 +1,88 @@
+import csv
 import os
 import logging
 import shutil
+import time
+
+LOGGER = logging.getLogger(__name__)
 
 
-def erase_file(path):
-    try:
-        os.remove(path)
-    except:
-        logging.error("Exception trying to remove the file: {}".format(path))
+class EraserStats:
+
+    error_files = []
+    error_directories = []
+
+    def append_file(self, path):
+        LOGGER.error('Error file: {}'.format(path))
+        self.error_file.append(path)
+
+    def append_directory(self, path):
+        LOGGER.error('Error directory: {}'.format(path))
+        self.error_directories.append(path)
+
+    def clean(self):
+        self.error_files = []
+        self.error_directories = []
+
+    def load_csv(self, path):
+        with open(path) as csvfile:
+            reader = csv.reader(csvfile)
+            for row in csv_reader:
+                if row[0] == 'file':
+                    self.error_files.append(row[1])
+                else:
+                    self.error_directories.append(row[1])
+
+    def save_csv(self, path):
+        with open(path, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            for efile in self.error_files:
+                writer.writerow(['file', efile])
+            for edirectory in self.error_directories:
+                writer.writerow(['directory', edirectory])
+
+    def __str__(self):
+        return 'Eraser Stats:\nError files: {}\nError directories: {}'.format(
+            len(self.error_files), len(self.error_directories))
 
 
-def erase_dir(path):
-    shutil.rmtree(path)
+class Eraser:
 
+    files = []
+    directories = []
+    stats = {
+        'files_error': [],
+        'dirs_error': [],
+    }
+
+    def targets(self, targets):
+        """Load targets to the files and directories lists."""
+        for target in targets:
+            if os.path.isfile(target):
+                self.files.append(target)
+            else:
+                self.directories.append(target)
+
+    def _rmtree_onerror(self, function, path, excinfo):
+        LOGGER.error('Error erasing path: "{}"'.format(path))
+        LOGGER.error('Error function: {}'.format(function))
+        LOGGER.error('Error excinfo:  {}'.format(excinfo))
+        self.stats['dirs_error'].append(path)
+
+    def _erase_file(self):
+        for f in self.files:
+            LOGGER.info('Erasing file: "{}" ...'.format(f))
+            try:
+                os.remove(f)
+                self.files.pop(f)
+            except:  # TODO: specify the exception
+                LOGGER.error('Error erasing file: "{}"'.format(f))
+                pass
+
+    def _erase_dir(self):
+        for dir in self.directories:
+            shutil.rmtree(dir, onerror=self._rmtree_onerror)
+
+    def erase(self):
+        self._erase_file()
+        self._erase_dir()
